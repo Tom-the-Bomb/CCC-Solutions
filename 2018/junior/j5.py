77837a563f91f607d005eae6464dbb6f763cf76a
Tom-the-Bomb <thello522@gmail.com>
2023-10-01 14:12:30 -0400

    s

--- 2018/junior/j5.py
+++ 2018/junior/j5.py
@@ -1,24 +1,25 @@
 from itertools import chain
 
 # working implementation
-def choose_your_own_path(input: str) -> str:
-    N, *pages = input.splitlines()
-    path_lengths = []
+class Solution:
+    def choose_your_own_path(input: str) -> str:
+        N, *pages = input.splitlines()
+        path_lengths = []
 
-    def find(page: str, length: int, visited: list[int]) -> None:
-        M, *targets = page.split()
-        if M == '0':
-            return path_lengths.append(length)
+        def find(page: str, length: int, visited: list[int]) -> None:
+            M, *targets = page.split()
+            if M == '0':
+                return path_lengths.append(length)
 
-        for i in targets:
-            i = int(i)
-            if i in visited:
-                return
-            find(pages[i - 1], length + 1, visited + [i])
+            for i in targets:
+                i = int(i)
+                if i in visited:
+                    return
+                find(pages[i - 1], length + 1, visited + [i])
 
-    find(pages[0], 1, [1])
-    not_accessed = set(chain(*[page.split()[1:] for page in pages])) == set(range(2, int(N) + 1))
-    return f'{("Y", "N")[not_accessed]}\n{min(path_lengths)}'
+        find(pages[0], 1, [1])
+        not_accessed = set(chain(*[page.split()[1:] for page in pages])) == set(range(2, int(N) + 1))
+        return f'{("Y", "N")[not_accessed]}\n{min(path_lengths)}'
 
 # Different implementations below:
 #
@@ -72,8 +73,3 @@ def choose_your_own_path_bfs(input: str) -> str:
 
     all_accessed = set(range(1, int(N) + 1)) == set(visited)
     return f'{("N", "Y")[all_accessed]}\n{step}'
-
-print(choose_your_own_path('''3
-2 2 3
-0
-1 1'''))
--- 2023/senior/s3.py
+++ 2023/senior/s3.py
@@ -7,12 +7,12 @@ class Solution:
         n, m, r, c = map(int, input.splitlines())
 
         poster = [['b'] * m for _ in range(n)]
-        
+
         if r == 1 and c == 1:
             poster[0] = ['a'] * c
             for i in range(r):
                 poster[0][i] = 'a'
         else:
             ...
-        
+
         return '\n'.join(' '.join(row) for row in poster)
